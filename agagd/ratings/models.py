f45275b120a2152aca68d68a130067e242351392
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-11-26 02:02:09 -0800

    add ratings app.  add models.  stub out some views.  add some fixture generation

---
+++ agagd/ratings/models.py
@@ -0,0 +1,45 @@
+from agagd_core.models import Member
+from django.db import models
+
+class GoServer(models.Model):
+    """Stores information about servers eligible to post game results to us."""
+    name = models.CharField(primary_key=True)
+    homepage = models.CharField(max_length=256)
+    api_key = models.CharField(max_length=1024)
+    game_url_root = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = u'go_server'
+
+
+class OnlineRating(models.Model): 
+    pin_player = models.ForeignKey(Member, db_column=u'Pin_Player', related_name='ratings_set', primary_key=True)
+    rating = models.FloatField(db_column=u'Rating')
+    sigma = models.FloatField(db_column=u'Sigma')
+    timestamp = models.DateField(db_column=u'Timestamp') # When the rating was calculated.
+    class Meta:
+        db_table = u'online_rating'
+
+
+class OnlinePlayer(models.Model):
+    """ Potentially many OP's per AGA member. """
+    pin_player = models.ForeignKey(Member, db_column=u'Pin_Player', primary_key=True)
+    server = models.ForeignKey(GoServer, db_column=u'Server', related_name='server_set')
+    server_secret_key = models.CharField(max_length=1024)
+    nickname = models.CharField(max_length=50)
+    class Meta:
+        db_table = u'online_player'
+
+
+class OnlineGame(models.Model): 
+    game_id = models.AutoField(primary_key=True, db_column=u'Game_ID') 
+    game_date = models.DateField(db_column=u'Game_Date') 
+    handicap = models.IntegerField(db_column=u'Handicap') 
+    komi = models.FloatField(db_column=u'Komi') 
+    pin_player_1 = models.ForeignKey(Member, db_column=u'Pin_Player_1', related_name='games_as_p1')
+    pin_player_2 = models.ForeignKey(Member, db_column=u'Pin_Player_2', related_name='games_as_p2') 
+    result = models.CharField(max_length=1, db_column=u'Result') 
+    sgf_url = models.CharField(max_length=256, db_column=u'Sgf_Code', blank=True) 
+    class Meta:
+        db_table = u'online_game'
+    
--- scripts/make_fake_fixtures.py
+++ scripts/make_fake_fixtures.py
@@ -1,15 +1,16 @@
 # A simple script to generate fake data
-import sys, random, json
+import sys, random, json, hashlib
 
-USAGE = 'Usage: python make_fake_fixtures.py [num_of_members] [num_of_games] [num_of_tournaments]'
+USAGE = 'Usage: python make_fake_fixtures.py [num_of_members] [num_of_games] [num_of_tournaments] [num_of_online_games]'
 GIVEN_NAMES = [ 'bruce', 'malcolm', 'kobe', 'peter', 'kaylee', 'inara', ]
 LAST_NAMES = [ 'lee', 'reynolds', 'bryant', 'parker', 'frye', 'serra', ]
 CHAPTER_CODES = ['FFLY', 'NBAG', 'DEAD', 'BEEF']
 COUNTRY_CODES = ['USA', 'CAN', 'JPN', 'KOR', 'CHN', 'TWN'] #these, oddly, are not the FK in the member table.
 COUNTRY_NAMES = ['An awesome country', 'A cool country', 'Canada', 'United States of Awesome', 'Donnybrookistan']
+SERVERS = ['JGS', 'KGS', 'LOLGS']
 import datetime as dt
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print USAGE
     quit()
 
@@ -17,6 +18,7 @@ try:
     member_count = int(sys.argv[1])
     game_count = int(sys.argv[2])
     tourney_count = int(sys.argv[3])
+    online_game_count = int(sys.argv[4])
 except ValueError:
     print USAGE
     quit()
@@ -121,4 +123,42 @@ for i, count_name in enumerate(COUNTRY_NAMES):
         }
     }) 
 
-print json.dumps(members + tournaments + games + chapters + countries, indent=4)
+servers = []
+for i, srv_name in enumerate(SERVERS): 
+    servers.append({
+        'pk': srv_name,
+        'model': 'ratings.server',
+        'fields': {
+            'name': srv_name,
+            'homepage': 'www.%s.com' % srv_name.lower(),
+            'api_key':  hashlib.sha224(srv_name).hexdigest(),
+            'game_url_root': 'not-a-url://www.%s.com/games' % srv_name.lower
+            }
+        })
+
+online_players = []
+alphabet = 'abcdefghijklmnopqrstuvwxyz' * 20
+nicks = {}
+for i in range(member_count):
+    mem = random.choice(members)['pk']
+    nick = random.sample(alphabet, 12)
+    if nick in nicks:
+        continue
+    nicks.add(nick)
+    online_player.append({
+        'pk': i,
+        'model': 'ratings.online_player',
+        'fields': {
+            'server': random.choice(SERVERS),
+            'server_secret_key': hashlib.sha224(nick).hexdigest(),
+            'nickname': nick,
+            'pin_player': mem,
+            }
+        })
+
+online_games = []
+for i in range(online_game_count):
+    pass 
+
+
+print json.dumps(members + tournaments + games + chapters + countries + servers + online_players, indent=4)

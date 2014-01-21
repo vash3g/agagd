55d4b1868a1341f53baad5573ced5607bef9f21d
Frank Lam <frank@duckpun.ch>
2014-01-20 16:27:52 -0800

    reversing players in games, adding tournaments

--- agagd/agagd_core/views/tables.py
+++ agagd/agagd_core/views/tables.py
@@ -1,14 +1,23 @@
 from django.core.paginator import Paginator
 from django.views.decorators.http import require_GET
 from agagd_core.json_response import json_view
-from agagd_core.models import Game
+from agagd_core.models import Game, Tournament
 from django.forms.models import model_to_dict
+from django.core.urlresolvers import reverse
 
 DEFAULT_PER_PAGE = 10
 
 @json_view
 def games(request):
-    return create_table(request, Game.objects.all())
+    table = create_table(request, Game.objects.all())
+    for game in table['results']:
+        game['player_1_details'] = reverse('member_detail', kwargs={'member_id': game['pin_player_1']})
+        game['player_2_details'] = reverse('member_detail', kwargs={'member_id': game['pin_player_2']})
+    return table
+
+@json_view
+def tournaments(request):
+    return create_table(request, Tournament.objects.all())
 
 @require_GET
 def create_table(request, queryset):

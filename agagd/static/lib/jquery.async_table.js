d438b487ff0e6d2398a57e1ece225748422953d2
Frank Lam <frank@duckpun.ch>
2014-01-29 02:57:06 -0800

    Rewrite of js async table lib

--- agagd/static/lib/jquery.async_table.js
+++ agagd/static/lib/jquery.async_table.js
@@ -1,43 +1,48 @@
-(function($) {
-    var table_template = [
+(function($, window) {
+    var table_template = _.template([
             "<table class='table table-striped table-hover'>",
-                "<thead><tr>HEADERS</tr></thead>",
-                "<tbody>CONTENTS</tbody>",
-            "</table>"
-        ].join(""),
-        row_template = "<tr>CONTENTS</tr>",
-        cell_template = "<td>CONTENTS</td>",
-        link_cell_template = "<td><a href='LINK'>LABEL</a></td>",
-        header_cell_template = "<th data-sort-key='SORT_KEY'>CONTENTS</th>",
-        loader_template = [
-            "<div id='TABLE_ID-loader'>",
-                "<div id='TABLE_ID-loader-block-1'></div>",
-                "<div id='TABLE_ID-loader-block-2'></div>",
-                "<div id='TABLE_ID-loader-block-3'></div>",
+                "<thead><tr>",
+                    "<% _.each(headers, function(header) { %>",
+                        "<th data-sort-key='<%= header.key %>'><%= header.label %></th>",
+                    "<% }); %>",
+                "</tr></thead>",
+                "<tbody>",
+                    "<% _.each(results, function(row) { %>",
+                        "<tr>",
+                        "<% _.each(headers, function(header) { %>",
+                            "<% if (row[header.key].type == 'link') { %>",
+                                "<td><a href='<%= row[header.key].link %>'><%= row[header.key].label %></a></td>",
+                            "<% } else { %>",
+                                "<td><%= row[header.key] %></td>",
+                            "<% } %>",
+                        "<% }); %>",
+                        "</tr>",
+                    "<% }); %>",
+                "</tbody>",
+            "</table>",
+            "<div class='paginator'>",
+                "<% if (page != 1) { %>",
+                    "<span class='previous'>Previous</span>",
+                "<% } %>",
+                "Page <%= page %> of <%= total_pages %>",
+                "<% if (page != total_pages) { %>",
+                    "<span class='next'>Next</span>",
+                "<% } %>",
             "</div>"
-        ].join(""),
-        failure_template = [
-            "<div id='TABLE_ID-failed-load'>",
-                "Something broke :(",
-            "</div>"
-        ].join(""),
-        previous_link_template = "<span class='previous'>Previous</span>",
-        next_link_template = "<span class='next'>Next</span>",
-        paginator_template = "<div class='paginator'>CONTENTS</div>",
-        cache = {},
-        deferred_go_to = {};
+        ].join("")),
+        deferred_go_to = {},
+        cache = {};
 
     $.async_table = {};
-
     $.async_table.init = function(config) {
         $(function() {
-            init_async_table(config);
+            cache[config.id] = new AsyncTable(config);
+            cache[config.id].reload();
         });
     };
-
     $.async_table.go_to_page = function(table_id, page, sort) {
-        var config = cache[table_id],
-            data = config? config.data : {};
+        var async_table = cache[table_id],
+            data = async_table? async_table.data : {};
 
         if (page) {
             data.page = parseInt(page);
@@ -51,138 +56,82 @@
             delete data.sort;
         }
 
-        if (config) {
-            config.reload();
+        if (async_table) {
+            async_table.reload();
         } else {
             deferred_go_to[table_id] = data;
         }
     };
 
-    function init_async_table(config) {
-        function check_config() {
-            var has_required_fields = config && config.id && config.remote_url;
+    function AsyncTable(config) {
+        var self = this,
+            $container = $("#" + config.id);
 
-            if (!has_required_fields) {
-                throw config.id + " has already been created!";
-            } else if (config.id in cache) {
-                throw "Config is missing some required fields!";
-            } else {
-                cache[config.id] = config;
+        // Required
+        self.remote_url = config.remote_url;
+        self.id = config.id;
+
+        // Optional
+        self.data = deferred_go_to[config.id] || config.data || {};
+        self.extra_parameters = config.extra_parameters || {};
+
+        // Methods
+        self.reload = function() {
+            var request_data = JSON.parse(JSON.stringify(self.data)), key;
+            for (key in self.extra_parameters) {
+                request_data[key] = self.extra_parameters[key];
             }
 
-            // Optional fields
-            config.data = deferred_go_to[config.id] || config.data || {};
-            config.extra_parameters = config.extra_parameters || {};
-            delete deferred_go_to[config.id];
-
-            // For external API
-            config.reload = load_remote_url;
-        }
-
-        function load_remote_url(data) {
-            var $container = $("#" + config.id), request_data, key;
-
-            function failed_load_view() {
-                $container.html(failure_template.replace(/TABLE_ID/g, config.id));
-            }
-            function loading_table_view() {
-                $container.html(loader_template.replace(/TABLE_ID/g, config.id));
-            }
-            function populate_table(response) {
-                var header_html = $.map(
-                        response.headers,
-                        function(header) {
-                            return header_cell_template.replace("CONTENTS", header.label).replace("SORT_KEY", header.key);
-                        }
-                    ).join(""),
-                    rows_html = $.map(
-                        response.results,
-                        function(cell) {
-                            var cells_html = $.map(
-                                response.headers,
-                                function(header) {
-                                    var value = cell[header.key];
-                                    if (value.type) {
-                                        if (value.type === "link") {
-                                            return link_cell_template.replace("LINK", value.link).replace("LABEL", value.label);
-                                        } else {
-                                            return cell_template.replace("CONTENTS", "");
-                                        }
-                                    } else {
-                                        return cell_template.replace("CONTENTS", value);
-                                    }
-                                }
-                            ).join("");
-
-                            return row_template.replace("CONTENTS", cells_html);
-                        }
-                    ).join(""),
-                    pages = [],
-                    pages_html;
-
-                config.current_page = parseInt(response.page);
-                config.total_pages = parseInt(response.total_pages);
-
-                if (config.current_page > 1) {
-                    pages.push(previous_link_template);
-                }
-                pages.push("<span>Page " + config.current_page + " of " + config.total_pages + "</span>");
-                if (config.current_page < config.total_pages) {
-                    pages.push(next_link_template);
-                }
-
-                pages_html = pages.join("");
-
-                $container.html([
-                    table_template.replace("HEADERS", header_html).replace("CONTENTS", rows_html),
-                    paginator_template.replace("CONTENTS", pages_html)
-                ].join(""));
-
-                // Add events
-                $container.find(".previous").click(function() {
-                    data.page = config.current_page - 1;
-                    load_remote_url(data);
-                });
-                $container.find(".next").click(function() {
-                    data.page = config.current_page + 1;
-                    load_remote_url(data);
-                });
-                $container.find("th").click(function() {
-                    var sort_key = $(this).attr("data-sort-key");
-                    if (sort_key === data.sort) {
-                        data.sort = "-" + sort_key;
-                    } else {
-                        data.sort = sort_key;
-                    }
-                    data.page = 1;
-                    load_remote_url(data);
-                });
-
-                // Trigger loaded events for external notifications
-                $(window).trigger({
-                    type: "async_table_loaded",
-                    table_id: config.id,
-                    sort: data.sort,
-                    page: data.page
-                });
-            }
-
-            data = data || config.data;
-            request_data = JSON.parse(JSON.stringify(data));
-            for (key in config.extra_parameters) {
-                request_data[key] = config.extra_parameters[key];
-            }
-
-            loading_table_view();
+            self.loading_table_view();
             $.ajax({
-                url: config.remote_url,
+                url: self.remote_url,
                 data: request_data,
-                success: populate_table,
-                error: failed_load_view
+                success: self.populate_table,
+                error: self.failed_load_view
             });
-        }
+        };
 
-        check_config();
-        load_remote_url();
+        self.populate_table = function(response) {
+            $container.html(table_template(response));
+
+            self.current_page = parseInt(response.page);
+            self.total_pages = parseInt(response.total_pages);
+
+            // Add events
+            $container.find(".previous").click(function() {
+                self.data.page = self.current_page - 1;
+                self.reload();
+            });
+            $container.find(".next").click(function() {
+                self.data.page = self.current_page + 1;
+                self.reload();
+            });
+            $container.find("th").click(function() {
+                var sort_key = $(this).attr("data-sort-key");
+                if (sort_key === data.sort) {
+                    self.data.sort = "-" + sort_key;
+                } else {
+                    self.data.sort = sort_key;
+                }
+                self.data.page = 1;
+                self.reload();
+            });
+
+            // Trigger loaded events for external notifications
+            $(window).trigger({
+                type: "async_table_loaded",
+                table_id: self.id,
+                sort: self.data.sort,
+                page: self.data.page
+            });
+        };
+
+        self.failed_load_view = function() {
+            // TODO Notify user of failure
+        };
+
+        self.loading_table_view = function() {
+            // TODO Loading view
+        };
     }
 })(jQuery, window);
